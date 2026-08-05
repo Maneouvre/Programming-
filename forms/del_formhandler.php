afb93be573f2f086d4b588170b3a6cb8b82fab58
maneouvre <emmanuellokumu93@gmail.com>
2026-08-05 17:44:22 +0300

    Initial commit

---
+++ forms/del_formhandler.php
@@ -0,0 +1,42 @@
+<?php
+var_dump($_SERVER["REQUEST_METHOD"]);
+//CHECKS IF SENT METHOD IS POST
+if ($_SERVER["REQUEST_METHOD"]==="POST"){
+            //gets the username and password from the form
+            $Username=($_POST["Username"]);
+            $password=($_POST["password"]);
+            $email=($_POST["email"]);
+
+        try {
+            //connects to the database
+            require_once("dbasehandler-inc.php");
+            //inserting data to mysql
+            $query="DELETE FROM users WHERE username=:Username AND pwd=:password;";
+            $stmt=$pdo->prepare($query);
+            $stmt->bindParam(":Username", $Username);
+            $stmt->bindParam(":password", $password);
+            
+            $stmt->execute();
+            $pdo=null;
+            $stmt=null;
+            header("Location:del_update_form.php");
+            die();
+
+            }
+        catch(PDOException  $e){
+            // If something goes wrong, stop and show the error
+                                die("Query Failed: ".$e->getMessage());
+                                };
+
+            if (empty($Username || $password)){
+                header("Location:del_update_form.php");
+
+            };
+    }
+   
+else
+    {header("Location:del_update_form.php");}
+
+
+
+?>
