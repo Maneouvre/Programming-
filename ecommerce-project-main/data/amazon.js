61009ef4bbe77b6adaacffc9b7633d7cf445f036
maneouvre <emmanuellokumu93@gmail.com>
2026-08-15 10:15:20 +0300

    finished login

--- ecommerce-project-main/data/amazon.js
+++ ecommerce-project-main/data/amazon.js
@@ -1,7 +1,6 @@
 import {cart, addToCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { convertCentsToDollar } from './products.js';
-
 function updateCartQuantity(){
                         let cartQuantity=0;
                         cart.forEach((item)=>{
